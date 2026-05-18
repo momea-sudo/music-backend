4efa17c3207a35995ea03ad1bf0e99d256c62001
momea-sudo <hassanmomea@gmail.com>
2026-05-17 21:49:07 -0500

    solve

--- server.js
+++ server.js
@@ -8,22 +8,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const mongoURI = process.env.MONGO_URI || "mongodb+srv://hassanmomea_db_user:ma8brGMHGLoXR6m2@cluster0.n7wcxlr.mongodb.net/biko_music?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI = process.env.MONGO_URI
 
 if (!mongoURI) {
   console.error("🚨 تحذير قاتل: السيرفر مش شايف متغير MONGO_URI في الإعدادات!");
 }
 
-// 🌐 الاتصال بقاعدة البيانات
+
 mongoose.connect(mongoURI)
   .then(() => console.log('🚀 تم الاتصال بنجاح بقاعدة بيانات MongoDB Atlas!'))
   .catch(err => {
     console.error('❌ فشل الاتصال بالمونجو:', err.message);
   });
 
-// ==========================================
-// 🏗️ [إضافة التعديل الأساسي] تعاريف جداول قاعدة البيانات (Schemas & Models)
-// ==========================================
+
 
 const TrackSchema = new mongoose.Schema({
   title: { type: String, required: true },
@@ -38,9 +36,6 @@ const VideoSchema = new mongoose.Schema({
 });
 const Video = mongoose.model('Video', VideoSchema);
 
-// ==========================================
-// 🛣️ الـ Routes (المسارات) الخاصة بالتراكات والفيديوهات
-// ==========================================
 
 app.get('/api/tracks', async (req, res) => {
   try { 
